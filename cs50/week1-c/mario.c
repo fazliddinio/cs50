3e87bc057ed797fe71354901c2db8a0045aab527
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-06 13:31:41 +0500

    cs50: complete mario with double pyramid

--- cs50/week1-c/mario.c
+++ cs50/week1-c/mario.c
@@ -8,16 +8,20 @@ int main(void) {
   } while (height < 1 || height > 8);
 
   for (int i = 0; i < height; i++) {
+    // Print spaces
     for (int j = 0; j < height - i - 1; j++) {
       printf(" ");
     }
+    // Print left pyramid
     for (int k = 0; k <= i; k++) {
+      printf("#");
+    }
+    // Print gap
+    printf("  ");
+    // Print right pyramid
+    for (int l = 0; l <= i; l++) {
       printf("#");
     }
     printf("\n");
   }
 }
-
-// loop patterns
-
-// TODO: add right side pyramid
