7ced0ee02b9ece73e395eefb776c9b91fcc24823
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-18 13:13:03 +0500

    bulbs: add emoji output for light bulbs

--- cs50/week2-arrays/bulbs.c
+++ cs50/week2-arrays/bulbs.c
@@ -8,12 +8,27 @@ void print_bulb(int bit);
 
 int main(void) {
   string message = get_string("Message: ");
-  // TODO: convert to binary
+
+  for (int i = 0, n = strlen(message); i < n; i++) {
+    int ascii = message[i];
+    int bits[BITS_IN_BYTE];
+
+    for (int j = BITS_IN_BYTE - 1; j >= 0; j--) {
+      bits[j] = ascii % 2;
+      ascii /= 2;
+    }
+
+    for (int j = 0; j < BITS_IN_BYTE; j++) {
+      print_bulb(bits[j]);
+    }
+    printf("\n");
+  }
 }
 
 void print_bulb(int bit) {
-  if (bit == 0) printf("0");
-  else printf("1");
+  if (bit == 0) {
+    printf("\U000026AB"); // Black circle emoji
+  } else if (bit == 1) {
+    printf("\U0001F7E1"); // Yellow circle emoji
+  }
 }
-
-// binary conversion logic
