d4711ac517ef7354beb085d70cf70dd79b2b5efb
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-19 11:03:08 +0500

    cs50: start caesar cipher problem

---
+++ cs50/week2-arrays/caesar.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[]) {
+  if (argc != 2) {
+    printf("Usage: ./caesar key\n");
+    return 1;
+  }
+  // TODO: validate key and encrypt
+  return 0;
+}
