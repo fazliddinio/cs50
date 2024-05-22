ac5e2def9aea93a0235072946e7710f0cb873ea9
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-22 10:33:50 +0500

    cs50: complete caesar cipher - all cases

--- cs50/week2-arrays/caesar.c
+++ cs50/week2-arrays/caesar.c
@@ -8,12 +8,27 @@ int main(int argc, string argv[]) {
     printf("Usage: ./caesar key\n");
     return 1;
   }
-  // TODO: validate key and encrypt
+
+  for (int i = 0, n = strlen(argv[1]); i < n; i++) {
+    if (!isdigit(argv[1][i])) {
+      printf("Usage: ./caesar key\n");
+      return 1;
+    }
+  }
+
+  int key = atoi(argv[1]);
+  string plaintext = get_string("plaintext: ");
+  printf("ciphertext: ");
+
+  for (int i = 0, n = strlen(plaintext); i < n; i++) {
+    if (isupper(plaintext[i])) {
+      printf("%c", (plaintext[i] - 'A' + key) % 26 + 'A');
+    } else if (islower(plaintext[i])) {
+      printf("%c", (plaintext[i] - 'a' + key) % 26 + 'a');
+    } else {
+      printf("%c", plaintext[i]);
+    }
+  }
+  printf("\n");
   return 0;
 }
-
-// validate argv[1] is numeric
-
-// lowercase working
-
-// approaches
