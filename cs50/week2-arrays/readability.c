0237869dbf76e919726b8c665fe7727f4641fa57
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-23 14:28:29 +0500

    cs50: start readability - counting functions

---
+++ cs50/week2-arrays/readability.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+
+int main(void) {
+  string text = get_string("Text: ");
+  int letters = count_letters(text);
+  int words = count_words(text);
+  printf("Letters: %i, Words: %i\n", letters, words);
+}
+
+int count_letters(string text) {
+  int count = 0;
+  for (int i = 0, n = strlen(text); i < n; i++)
+    if (isalpha(text[i])) count++;
+  return count;
+}
+
+int count_words(string text) {
+  int count = 1;
+  for (int i = 0, n = strlen(text); i < n; i++)
+    if (text[i] == ' ') count++;
+  return count;
+}
