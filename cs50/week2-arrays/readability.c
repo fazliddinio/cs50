7794d0bbcac3e195bfa5719c7290f482f2c7fd41
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-25 12:48:55 +0500

    readability: implement Coleman-Liau formula

--- cs50/week2-arrays/readability.c
+++ cs50/week2-arrays/readability.c
@@ -6,26 +6,55 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void) {
   string text = get_string("Text: ");
+
   int letters = count_letters(text);
   int words = count_words(text);
-  printf("Letters: %i, Words: %i\n", letters, words);
+  int sentences = count_sentences(text);
+
+  float L = (float)letters / words * 100;
+  float S = (float)sentences / words * 100;
+
+  int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+  if (index < 1) {
+    printf("Before Grade 1\n");
+  } else if (index >= 16) {
+    printf("Grade 16+\n");
+  } else {
+    printf("Grade %i\n", index);
+  }
 }
 
 int count_letters(string text) {
   int count = 0;
-  for (int i = 0, n = strlen(text); i < n; i++)
-    if (isalpha(text[i])) count++;
+  for (int i = 0, n = strlen(text); i < n; i++) {
+    if (isalpha(text[i])) {
+      count++;
+    }
+  }
   return count;
 }
 
 int count_words(string text) {
   int count = 1;
-  for (int i = 0, n = strlen(text); i < n; i++)
-    if (text[i] == ' ') count++;
+  for (int i = 0, n = strlen(text); i < n; i++) {
+    if (text[i] == ' ') {
+      count++;
+    }
+  }
   return count;
 }
 
-// count_sentences added
+int count_sentences(string text) {
+  int count = 0;
+  for (int i = 0, n = strlen(text); i < n; i++) {
+    if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+      count++;
+    }
+  }
+  return count;
+}
