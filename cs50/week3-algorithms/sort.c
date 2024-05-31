20381bf034ed9e569676043988190a1cd6a095ab
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-05-31 13:16:21 +0500

    sort: add early termination optimization

--- cs50/week3-algorithms/sort.c
+++ cs50/week3-algorithms/sort.c
@@ -6,16 +6,38 @@ void swap(int *a, int *b);
 int main(void) {
   int arr[] = {64, 34, 25, 12, 22, 11, 90};
   int n = 7;
-  // bubble sort
-  for (int i = 0; i < n - 1; i++)
-    for (int j = 0; j < n - i - 1; j++)
-      if (arr[j] > arr[j + 1])
+
+  printf("Original array: ");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
+  // Bubble Sort
+  for (int i = 0; i < n - 1; i++) {
+    bool swapped = false;
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
         swap(&arr[j], &arr[j + 1]);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+
+  printf("Sorted array: ");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
   return 0;
 }
 
 void swap(int *a, int *b) {
-  int temp = *a; *a = *b; *b = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
-
-// added output
