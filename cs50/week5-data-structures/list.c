0ce5200d65ccd7048bd106bba405ffa49cd1006e
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-04 11:20:18 +0500

    cs50 week5: linked list in C - node struct

---
+++ cs50/week5-data-structures/list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  int number;
+  struct node *next;
+} node;
+
+int main(void) {
+  node *list = NULL;
+  // TODO: build list from argv
+  return 0;
+}
