83539850ae0a8ef166b5bd0b0be81f86b0985605
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-07 17:47:50 +0500

    cs50: complete linked list with proper cleanup

--- cs50/week5-data-structures/list.c
+++ cs50/week5-data-structures/list.c
@@ -1,3 +1,4 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,12 +7,37 @@ typedef struct node {
   struct node *next;
 } node;
 
-int main(void) {
+int main(int argc, char *argv[]) {
   node *list = NULL;
-  // TODO: build list from argv
+
+  for (int i = 1; i < argc; i++) {
+    int number = atoi(argv[i]);
+
+    node *n = malloc(sizeof(node));
+    if (n == NULL) {
+      return 1;
+    }
+    n->number = number;
+    n->next = NULL;
+
+    // Insert at beginning
+    n->next = list;
+    list = n;
+  }
+
+  // Print the list
+  printf("List: ");
+  for (node *ptr = list; ptr != NULL; ptr = ptr->next) {
+    printf("%i ", ptr->number);
+  }
+  printf("\n");
+
+  // Free memory
+  while (list != NULL) {
+    node *tmp = list->next;
+    free(list);
+    list = tmp;
+  }
+
   return 0;
 }
-
-// insert at head
-
-// print and free
