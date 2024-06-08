78fb6467f3759905c88aeeddbd4b290f497ef683
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-08 10:05:38 +0500

    cs50: start dictionary - hash table struct

---
+++ cs50/week5-data-structures/dictionary.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASH_SIZE 26
+
+typedef struct node {
+  char word[46];
+  struct node *next;
+} node;
+
+node *table[HASH_SIZE];
+unsigned int word_count = 0;
+
+unsigned int hash(const char *word) { return toupper(word[0]) - 'A'; }
+
+// TODO: load, check, size, unload
