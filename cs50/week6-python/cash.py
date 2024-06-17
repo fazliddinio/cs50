4fab9db8e73941e6245e68da291bb31137d04a91
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-17 14:24:17 +0500

    cs50 week6: start cash.py - input validation

---
+++ cs50/week6-python/cash.py
@@ -0,0 +1,16 @@
+def main():
+    while True:
+        try:
+            change = float(input("Change owed: "))
+            if change >= 0:
+                break
+        except ValueError:
+            pass
+
+    cents = round(change * 100)
+    coins = 0
+    # TODO: calculate minimum coins
+    print(coins)
+
+if __name__ == "__main__":
+    main()
