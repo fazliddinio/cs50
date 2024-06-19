5e46803eb4dc7718011e4e314ae226e1b2910b37
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-19 12:17:16 +0500

    cs50: complete cash.py - works perfectly

--- cs50/week6-python/cash.py
+++ cs50/week6-python/cash.py
@@ -1,4 +1,5 @@
 def main():
+
     while True:
         try:
             change = float(input("Change owed: "))
@@ -9,10 +10,24 @@ def main():
 
     cents = round(change * 100)
     coins = 0
-    # TODO: calculate minimum coins
+
+    # Quarters (25 cents)
+    coins += cents // 25
+    cents %= 25
+
+    # Dimes (10 cents)
+    coins += cents // 10
+    cents %= 10
+
+    # Nickels (5 cents)
+    coins += cents // 5
+    cents %= 5
+
+    # Pennies (1 cent)
+    coins += cents
+
     print(coins)
+
 
 if __name__ == "__main__":
     main()
-
-# quarters dimes nickels pennies
