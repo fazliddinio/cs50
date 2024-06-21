89778adcb249fd1ff6059c4e101e34265356a221
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-21 11:44:23 +0500

    cs50: start dna.py - file reading setup

---
+++ cs50/week6-python/dna.py
@@ -0,0 +1,10 @@
+import csv
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python dna.py data.csv sequence.txt")
+    # TODO: read database, find matches
+
+if __name__ == "__main__":
+    main()
