406bf79fa108bee753f39e73186ca294e5c2d9d9
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-06-29 13:29:26 +0500

    finance.sql: add indexes for performance

--- cs50/week7-sql/finance.sql
+++ cs50/week7-sql/finance.sql
@@ -4,4 +4,24 @@ CREATE TABLE users (
     hash TEXT NOT NULL
 );
 
--- stocks and transactions tables
+CREATE TABLE stocks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    symbol TEXT NOT NULL,
+    shares INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    symbol TEXT NOT NULL,
+    shares INTEGER NOT NULL,
+    price NUMERIC NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    type TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE INDEX idx_user_stocks ON stocks(user_id);
+CREATE INDEX idx_user_transactions ON transactions(user_id);
