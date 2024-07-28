56e034bcad4bdc1b28cabe3e4845c27b7ec3c400
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-07-28 13:06:07 +0500

    python: file I/O - reading and writing text

---
+++ python/basics/05_file_io.py
@@ -0,0 +1,11 @@
+# file I/O practice
+import json
+import os
+
+def write_text_file(filename, content):
+    with open(filename, "w") as f:
+        f.write(content)
+
+def read_text_file(filename):
+    with open(filename, "r") as f:
+        return f.read()
