e9a7a5484a05e89904f1f05d4e54ef9d29a46c06
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-08-21 10:56:02 +0500

    project: number guessing game with hints

---
+++ projects/13-number-guessing/guess.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Number Guessing Game"""
+import random
+
+def play():
+    secret = random.randint(1, 100)
+    print("Guess a number between 1 and 100")
+    while True:
+        guess = int(input("Your guess: "))
+        if guess == secret:
+            print("Correct!")
+            break
+        print("Too low!" if guess < secret else "Too high!")
+
+if __name__ == "__main__":
+    play()
