58489f024c43303150af0f6102cacb03f30c00b3
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-09-01 10:18:08 +0500

    project: start password generator

---
+++ projects/03-password-generator/passgen.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Password Generator"""
+import secrets
+import string
+
+def generate_password(length=16):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return "".join(secrets.choice(chars) for _ in range(length))
+
+if __name__ == "__main__":
+    print(generate_password())
