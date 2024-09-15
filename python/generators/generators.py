351a04f88cf4e1d3de8c4f6f7952703413da2913
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-09-15 10:57:53 +0500

    python: generators basics - yield keyword

---
+++ python/generators/generators.py
@@ -0,0 +1,12 @@
+# generators and iterators
+import sys
+from typing import Generator, Iterator
+
+def count_up_to(max_value):
+    count = 1
+    while count <= max_value:
+        yield count
+        count += 1
+
+for n in count_up_to(5):
+    print(n)
