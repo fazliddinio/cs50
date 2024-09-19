02599a3b0ae695a91c2f25a7d368b10a96f2d7af
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-09-19 11:28:51 +0500

    project: file organizer - sort files by type

---
+++ projects/06-file-organizer/organizer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""File Organizer"""
+from pathlib import Path
+import shutil
+
+FILE_TYPES = {
+    "Images": [".jpg",".png",".gif"],
+    "Documents": [".pdf",".doc",".txt"],
+    "Code": [".py",".js",".c"],
+}
+
+def get_category(ext):
+    for cat, exts in FILE_TYPES.items():
+        if ext.lower() in exts: return cat
+    return "Other"
