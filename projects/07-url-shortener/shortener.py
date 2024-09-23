e3bdb26a7799835b43704f63fe5fb6aef2feb4d5
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-09-23 10:57:22 +0500

    project: URL shortener with hash codes

---
+++ projects/07-url-shortener/shortener.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""URL Shortener"""
+import hashlib
+import json
+from pathlib import Path
+
+DB_FILE = Path.home() / ".url_shortener.json"
+
+def shorten(url):
+    code = hashlib.md5(url.encode()).hexdigest()[:6]
+    print(f"Short: https://short.url/{code}")
+    return code
