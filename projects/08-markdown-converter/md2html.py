2d151658ff18a597a749b690f255c20ff32b63ee
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-09-27 11:28:23 +0500

    project: markdown to HTML converter

---
+++ projects/08-markdown-converter/md2html.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Markdown to HTML"""
+import re
+
+def convert_headers(text):
+    for i in range(6, 0, -1):
+        pattern = r"^#{" + str(i) + r"}\s+(.+)$"
+        text = re.sub(pattern, f"<h{i}>\\1</h{i}>", text, flags=re.MULTILINE)
+    return text
