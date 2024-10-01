43e1e944d389f7f3fa1385dbc10b645444a96f38
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-10-01 10:21:08 +0500

    python: design patterns - singleton

---
+++ python/design_patterns/patterns.py
@@ -0,0 +1,10 @@
+# design patterns
+from abc import ABC, abstractmethod
+from typing import List, Any
+
+class Singleton:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
