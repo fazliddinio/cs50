39d15b5a555fd65de934780dee3fd17c0b7e2e02
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-10-07 14:54:31 +0500

    testing: parametrize and exception testing

--- python/testing/test_examples.py
+++ python/testing/test_examples.py
@@ -8,3 +8,5 @@ class TestAdd:
     def test_positive(self): assert add(2, 3) == 5
     def test_negative(self): assert add(-1, -1) == -2
     def test_zero(self): assert add(0, 0) == 0
+
+# parametrize
