c0dedfd9f4cfea5f14bee4746b0ee9f341372081
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-11-01 10:10:51 +0500

    algorithms: bubble sort implementation

---
+++ algorithms/sorting/sorting.py
@@ -0,0 +1,15 @@
+"""Sorting Algorithms"""
+from typing import List
+
+def bubble_sort(arr):
+    """Bubble Sort - O(n^2)"""
+    n = len(arr)
+    arr = arr.copy()
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped: break
+    return arr
