3856ab43322a382c96688277d045967500947aac
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-11-10 10:00:24 +0500

    data structures: linked list - Node and append

---
+++ algorithms/data-structures/structures.py
@@ -0,0 +1,21 @@
+"""Data Structures"""
+from typing import Any, Optional, List
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self._size = 0
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            curr = self.head
+            while curr.next: curr = curr.next
+            curr.next = new_node
+        self._size += 1
