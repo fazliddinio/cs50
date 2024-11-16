37a206560e79e834041811b9559ffc686e8dc07c
Fazliddin Mamutkhanov <fmamutkhanov@gmail.com>
2024-11-16 10:40:07 +0500

    cli-tool: file finder with glob patterns

---
+++ cli-tools/file-finder/finder.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""File Finder"""
+from pathlib import Path
+
+def find_files(directory, pattern="*", recursive=True):
+    glob_pat = f"**/{pattern}" if recursive else pattern
+    for f in Path(directory).glob(glob_pat):
+        if f.is_file(): yield f
